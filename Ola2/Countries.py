c7339a8a1d680982d7a75feb4121dc3806c4b247
Mads Knudsen <madsfknudsen@hotmail.com>
2024-03-20 14:34:43 +0100

    Country and City done

--- Ola2/Countries.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-cluster = MongoClient("mongodb+srv://user:password420@ola2cluster.bijj3hs.mongodb.net/")
-
-db = cluster["ola2db"]
-collection = db["Countries"]
-
-collection.insert_one({"_id":0, "name":"Test"})
